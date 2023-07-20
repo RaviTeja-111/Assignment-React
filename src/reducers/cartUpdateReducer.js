afa98178eb1f01ffa301042dd63221b6bd045152
RaviTeja-111 <kommuriraviteja111.com>
2023-07-20 17:37:04 +0530

    updated the cart toggle using cartDisplayReducer and cartUpdateReducer

---
+++ src/reducers/cartUpdateReducer.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+    name : 'cartDetails',
+    initialState : {
+            
+    },
+    reducers : {
+        addItemtoCart(){},
+        removeItemfromCart(){}
+    }
+})
