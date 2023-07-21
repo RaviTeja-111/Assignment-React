a8747a30d045a8c5f3da9e71f01aa41f64fafd2a
RaviTeja-111 <kommuriraviteja111.com>
2023-07-21 19:29:41 +0530

    Sending data to the backend using Firebase url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,6 +7,14 @@ import Products from './components/Shop/Products';
 
 function App() {
   const disp = useSelector(state => state.cartDisplay.showCart);
+  const cart = useSelector(state => state.cartUpdate);
+
+  useEffect(() => {
+    fetch('https://for-react-78799-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart])
   return (
     <Layout>
       {disp && <Cart />}
